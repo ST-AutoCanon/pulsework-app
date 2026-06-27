39aa6803f91a520d6cd7a7d2c3dca23f8e991dda
vaibhavichinchure <vaibhavi9sukalpatech@gmail.com>
2026-06-27 17:07:06 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,10 @@ import { useEffect, useState } from "react";
 export default function Home() {
 
   const OFFICE = {
-    latitude:   17.336252656963396,
-    longitude: 76.85817976382933,
+    latitude:   17.33563195085764,
+    longitude: 76.85610010687031,
     radius: 100
   };
-
   
 
   const [location,setLocation] = useState({
